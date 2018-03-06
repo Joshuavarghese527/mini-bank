0979f8b85f205c1500c211d0b075e4a3111e287d
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-03-05 20:10:18 -0500

    Changed Greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
+  res.send({ bye: 'mini-bank buddy'});
 });
 
 const PORT = process.env.PORT || 5000;
