e54b93606929e5472470c4ab5d92e0c9bf2da5c4
Joshuavarghese527 <joshuavarghese527@gmail.com>
2018-03-06 12:30:39 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Mini-Bank App
+
+## Key requirement: 
+
+1. A login page, which authenticates a user through the Plaid API Plaid's API authenticates users using their banking credentials and pulls in all their banking transaction data. Make sure your login page includes the necessary fields to pull in the transaction history.
+You should be using the sandbox credentials to make the appropriate API call to retrieve all the data necessary to complete the task.
+
+2. A dashboard/homepage or pages to display the data. You should display both relevant account data and transaction data per account.
+Display the data in any way you choose, the goal is to show the history of transactions and the data relevant to a user.
+
+3.  A way to sort the transaction data There should be a feature to sort the data in ways that you find relevant to a user. We hope that you will use any knowledge of product or UX to embellish on this portion of the task. 
+
+## Server (ExpressJS Side)
+
+- Plaid API 
+- MongooseDB ? 
+
+## Client (React Side):
+
+- Create login page
+- Dashboard UI/UX
+- Transaction Data Sorted
+- Redux
+
+
+## Having Backend Server and Frontend Server run on one command:
+
+x Npm run dev
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const plaid = require('plaid');
+const moment = require('moment');
+const keys = require('./config/keys')
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+var ACCESS_TOKEN = null;
+var PUBLIC_TOKEN = null;
+var ITEM_ID = null;
+
+// Initialize the Plaid client
+var client = new plaid.Client(
+  keys.PLAID_CLIENT_ID,
+  keys.PLAID_SECRET,
+  keys.PLAID_PUBLIC_KEY,
+  plaid.environments[keys.PLAID_ENV]
+);
+
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'mini-bank buddy'});
-});
-
 const PORT = process.env.PORT || 5000;
+
+
+app.post('/get_access_token', function(request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      console.log('Could not exchange public_token!' + '\n' + error);
+      return response.json({error: msg});
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    console.log('Item ID: ' + ITEM_ID);
+    response.json({'error': false});
+  });
+});
 app.listen(PORT);
